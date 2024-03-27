ace6926fead0ab49885ae12e2384e532c74267f8
md5dalton <md5daltonggs@gmail.com>
2024-03-27 20:41:22 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,19 +2,23 @@ import { Roboto } from "next/font/google"
 import "./globals.css"
 
 const font = Roboto({ 
-  subsets: ["latin"],
-  weight: ["400", "700"]
+    subsets: ["latin"],
+    weight: ["400", "700"]
 })
 
 export const metadata = {
-  title: "Newsletter",
-  description: "Newsletter sign-up form with success message",
+    title: "Newsletter",
+    description: "Newsletter sign-up form with success message",
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={font.className}>
+                <main>
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
 }
